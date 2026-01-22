0dbec8c7e7784dc17513dc1e34958a531ef093e1
rc808129 <rc808129@gmail.com>
2026-01-22 18:41:57 +0530

    commits

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
 import React from "react";
 import './../styles/App.css';
+import { Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Navigation from "./Navigation";
+import About from "./About";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+       
+            <Navigation />
+        <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
     </div>
   )
 }
